37dd0fc5104428d02eeab2c70b48e348ce93fb77
gitcuber369 <arpitchaudhary2003@gmail.com>
2024-09-09 23:28:16 +0530

    added some component for the landing page

--- app/(landingpage)/layout.tsx
+++ app/(landingpage)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LandingPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-neutral-950">
+    <div className="min-h-screen bg-slate-100">
       <main className="pt-40 pb-20">{children}</main>
     </div>
   );
--- app/(landingpage)/page.tsx
+++ app/(landingpage)/page.tsx
@@ -5,11 +5,18 @@ function LandingPage() {
   return (
     <div className="flex items-center justify-center flex-col">
       <div className="flex items-center justify-center flex-col">
-        <div>
-          hey there
-          <Medal />
+        <div className="mb-4 flex items-centre p-3 shadow-sm border bg-violet-200 text-violet-700 rounded-full">
+          <Medal className="h-6 w-6 mr-2" />
+          No. 1 Task Manager
+        </div>
+        <h1 className="lg:text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          KanFlow helps teams move
+        </h1>
+        <div className="lg:text-3xl md:text-6xl bg-gradient-to-r to-violet-600 from-blue-600 text-white w-fit  px-4 p-2 rounded-md pb-4">
+          work to production.
         </div>
       </div>
+      <div>Collaborate, track, and manage your work with KanFlow, from high rises to the home </div>
     </div>
   );
 }
