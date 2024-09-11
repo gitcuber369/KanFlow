5db08c607bb9799bd27944e32777fa93b7007f16
gitcuber369 <arpitchaudhary2003@gmail.com>
2024-09-11 18:36:52 +0530

    added footer to the landing page

--- app/(landingpage)/_components/footer.tsx
+++ app/(landingpage)/_components/footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 w-full h-10 px-4 border-t bg-slate-100">
+    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mt-1  mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
