488b20050ebd0819bd1cf354eaf82d0eefc1b995
gitcuber369 <arpitchaudhary2003@gmail.com>
2024-09-21 21:46:47 +0530

    prisma init

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,15 @@
 const OrganizationIdPage = () => {
-  return <div className="w-full mb-20">Organization</div>;
+  return (
+    <div className="w-full mb-20">
+      <form action="">
+        <input 
+        type="text"
+        required
+        name="text"
+        placeholder="" />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
